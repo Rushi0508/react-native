0d170ac3e14a61c06e76cff8dbd941f90a085fba
Rushi_0508 <rushigandhi14@gmail.com>
2023-11-17 12:25:49 +0530

    App-7: coded logic for tictactoe

---
+++ tictactoe/src/App.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Snackbar from 'react-native-snackbar';
+import Icons from './components/Icons';
+
+function App(): JSX.Element {
+
+  const [isCross, setIsCross] = useState(false);
+  const [gameWinner, setGameWinner] = useState('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const reloadGame = ()=>{
+    setIsCross(false)
+    setGameState(new Array(9).fill('empty', 0, 9))
+    setGameWinner('')
+  }
+
+  const checkIsWinner = () => {
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game! 🥳`);
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game... ⌛️');
+    }
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <View>
+        <Text>
+
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
